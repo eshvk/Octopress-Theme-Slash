081d6345c7a9801fcd1014955c70e5750beaa6b9
esh <eshwaran@utexas.edu>
2012-04-25 18:15:28 -0700

    Fixed javascript to open links within current domain in same window

--- source/javascripts/slash.js
+++ source/javascripts/slash.js
@@ -1,12 +1,30 @@
 (function($){
 	/* external.js */
 	var host = location.host;
-
-	$('a').on('click', function(e){
-		var href = $(this).attr('href'),
-			link = href.replace(/(https?:\/\/)(.*)\/(.*)/, '$2');
-
-		if (href.match('https?') && link != host){
+    // Replace any trailing and leading spaces
+    var tld = host.replace(/^\s+/, "");
+    tld = tld.replace(/\s+$/, "");
+    // Remove http/https/ftp
+    tld = tld.replace(new RegExp(/^http\:\/\/|^https\:\/\/|^ftp\:\/\//i),"");
+    // Strip away www
+    tld = tld.replace(new RegExp(/^www\./i),"");
+    var parts = tld.split('.');
+    var type = parts.pop();
+    var domain = parts.pop();
+    var tld = domain + '.' + type;
+   	$('a').on('click', function(e){
+        var href = $(this).attr('href');
+        // Remove http/https/ftp
+        var link = href.replace(new RegExp(/^http\:\/\/|^https\:\/\/|^ftp\:\/\//i),"");
+        // Remove stuff after first forward slash
+        link = link.replace(new RegExp(/\/(.*)/),"");
+        // Strip away www
+        link = link.replace(new RegExp(/^www\./i),"");
+        var parts = link.split('.');
+        var type = parts.pop();
+        var domain = parts.pop();
+        var curr_tld = domain + '.' + type;
+		if (href.match('https?') && curr_tld != tld){
 			window.open(href);
 			e.preventDefault();
 		}
